a39e7fade7428c0f8aa0708bfb61b7e66b5049ea
davideploy <deehzi@pm.me>
2022-08-06 17:20:18 -0300

    criando o pesquisador

--- app/src/main/java/com/davideploy/repository/searcher/ui/MainActivity.kt
+++ app/src/main/java/com/davideploy/repository/searcher/ui/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.davideploy.repository.searcher.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import com.davideploy.repository.searcher.R
+import com.davideploy.repository.searcher.core.createDialog
+import com.davideploy.repository.searcher.core.createProgressDialog
+import com.davideploy.repository.searcher.core.hideSoftKeyboard
 import com.davideploy.repository.searcher.databinding.ActivityMainBinding
+import com.davideploy.repository.searcher.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
+    private val dialog by lazy { createProgressDialog() }
+    private val viewModel by viewModel<MainViewModel>()
+    private val adapter by lazy { RepoListAdapter() }
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +25,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        binding.rvRepos.adapter = adapter
+
+        viewModel.repos.observe(this) {
+            when (it) {
+                MainViewModel.State.Loading -> dialog.show()
+                is MainViewModel.State.Error -> {
+                    createDialog {
+                        setMessage(it.error.message)
+                    }.show()
+                    dialog.dismiss()
+                }
+                is MainViewModel.State.Success -> {
+                    dialog.dismiss()
+                    adapter.submitList(it.list)
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        query?.let { viewModel.getRepoList(it) }
+        binding.root.hideSoftKeyboard()
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText")
+        return false
+    }
+
+    companion object {
+        private const val TAG = "TAG"
     }
 
 }
